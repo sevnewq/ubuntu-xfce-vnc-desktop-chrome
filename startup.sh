c31c6ea6c3ed6f2103e060d49dfd5e87bf4d0217
Sev Huang <sevnew@gmail.com>
2015-11-19 16:18:47 +0800

    first commit

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# prepare ssh server
+mkdir -p /var/run/sshd
+
+# start up supervisord, all daemons should launched by supervisord.
+/usr/bin/supervisord -c /root/supervisord.conf
+
+# start a shell
+/bin/bash
+
+# open google chrome
+#google-chrome-stable --user-data-dir=/root
+
+ping 8.8.8.8
